09cb427ca926fcbc7a7bb4f512685f7f17d1a4ed
aurora0x27 <qingfengzl0831@163.com>
2026-04-12 18:59:14 +0800

    Fix import

--- lua/popup/init.lua
+++ lua/popup/init.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------
 -- Cmdline and InputBox
 --------------------------------------------------------------------------------
-local Win = require 'ui.window'
+local Win = require 'popup.window'
 local M = {}
 
 ---@class MatchDecl
